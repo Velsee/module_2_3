6c5339c84b19fdab1d0ec172218bd744caffb4fc
Valeriya <113857212+Velsee@users.noreply.github.com>
2024-10-27 20:56:34 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,13 +1,19 @@
+# Исходные данные
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print("Исходные данные:\nmy_list = ", my_list)
-index = 0
 
-print("\nВывод на консоль:")
-while index < len(my_list):
-    if my_list[index] >= 0:
-        if my_list[index] != 0:
-            print(my_list[index])
-        index += 1
-        continue
-    else:
+# Начальное значение индекса
+counter = 0
+
+# Цикл while для перебора элементов списка
+while counter < len(my_list):
+    current_number = my_list[counter]
+
+    if current_number < 0:  # Оператор break при встрече отрицательного числа
         break
+    elif current_number == 0:  # Оператор continue для игнорирования нуля
+        counter += 1
+        continue
+    else:  # Если число положительное
+        print(current_number)
+
+    counter += 1  # Увеличиваем индекс для перехода к следующему элементу
